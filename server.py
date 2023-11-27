3f1f0f8d15aa0d100f8ffef6b6ef7101abd10644
susjunyou <12110911@mail.sustech.edu.cn>
2023-11-27 17:49:23 +0800

    update server

--- server.py
+++ server.py
@@ -18,24 +18,16 @@ if __name__ == '__main__':
 
     # 创建一个 socket 对象
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # 绑定到指定的端口
     server_socket.bind((host, int(port)))
-
     # 设置最大连接数
     server_socket.listen(5)
-
-    print(f"Server is listening on port {port}")
-
     while True:
         # 等待客户端连接
         client_socket, addr = server_socket.accept()
-
-        print(f"Got a connection from {addr}")
-
         # 接收客户端发送的数据
         data = client_socket.recv(1024)
-        print(f"Received data: {data.decode('utf-8')}")
+        print(data.decode('utf-8'))
 
         # 发送HTTP响应
         response = 'HTTP/1.0 200 OK\n\nThank you for connecting'
